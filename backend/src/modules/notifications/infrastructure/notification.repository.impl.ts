a7df37d1307c454f7b9d56570c5f0de562bdb3de
Arthur Rocha <arthurrocha007.net@gmail.com>
2026-02-10 20:49:03 -0300

    feat(notifications): adiciona suporte ao prisma

--- backend/src/modules/notifications/infrastructure/notification.repository.impl.ts
+++ backend/src/modules/notifications/infrastructure/notification.repository.impl.ts
@@ -2,23 +2,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { INotificarionRepository } from '../domain/notification.repository';
 import { Injectable } from '@nestjs/common';
 import { Notification } from '../domain/notification.entity';
+import { NotificationMapper } from './notification.mapper';
 
 @Injectable()
 export class NotificationRepository implements INotificarionRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async findAll(): Promise<Notification[]> {
-    const notifications = Promise.resolve([
-      new Notification(
-        '1',
-        'typescript',
-        'hello world',
-        'LOW',
-        '2',
-        new Date(),
-      ),
-    ]);
-
-    return await notifications;
+    const notifications = await this.prisma.notification.findMany();
+    return notifications.map((notification) =>
+      NotificationMapper.toDomain(notification),
+    );
   }
 }
