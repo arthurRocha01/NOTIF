8abe58e5866094b66da1825a11b354449d9390a0
thdev07 <joaothales90@gmail.com>
2026-03-01 22:31:29 -0300

    Ajustes finais antes do pull

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -14,7 +14,7 @@ class NotifApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NOTIF Portal',
+      title: 'NOTIF ',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
